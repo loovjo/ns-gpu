6d003e6c262c8dd634074312446b5d424319847d
loovjo <jonathan.loov@gmail.com>
2020-09-10 22:40:18 +0200

    Prelude

--- compile-shaders.sh
+++ compile-shaders.sh
@@ -11,6 +11,10 @@ RESET="\033[0m"
 for file in $SHADERS_DIR/*
 do
     filename=$(basename $file)
+    if [ $filename = "prelude.comp" ]
+    then
+        continue
+    fi
     basename=${filename%.*}
     format=${filename#*.}
     out="$BUILD_DIR/$basename-$format.spv"
