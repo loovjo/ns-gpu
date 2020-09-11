86f12e52c22a71bf8379ad313ed8e98e710122b5
loovjo <jonathan.loov@gmail.com>
2020-09-11 18:44:43 +0200

    Remove previous animation, logging

--- render-animation.sh
+++ render-animation.sh
@@ -3,7 +3,9 @@ rm image-dumps/*.png
 ./compile-shaders.sh && cargo run
 if [ $? -eq 0 ]
 then
+    echo 'Encoding gif'
     cd image-dumps
+    rm animation.gif
     files_in_order=$(ls -rt)
     convert -delay 12 -loop 0 $files_in_order animation.gif
     cd ..
