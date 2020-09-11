5c7bd79510617d31b29c4a8842764569742d1765
loovjo <jonathan.loov@gmail.com>
2020-09-11 18:22:41 +0200

    Render animation script

---
+++ render-animation.sh
@@ -0,0 +1,10 @@
+./compile-shaders.sh && cargo run
+if [ $? -eq 0 ]
+then
+    cd image-dumps
+    files_in_order=$(ls -rt)
+    convert -delay 12 -loop 0 $files_in_order animation.gif
+    cd ..
+else
+    echo "Error :("
+fi
